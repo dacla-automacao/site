0d4ea3466bffd0615d6ff990821b41e65fadb6da
Daniel Clarinda <dacla.automacao@hotmail.com>
2021-03-31 10:21:35 -0300

    Update

--- main/regra.js
+++ main/regra.js
@@ -1,18 +1,10 @@
 let sobre = document.getElementById("texto").innerHTML
-let produtos = `
-<h1>Produtos</h1>
-<p><strong>#1 - Automatização do processo industrial de fabricação de Paver e Lajotas.</strong></p>
-
-<p>Nesse projeto foi desenvolvido uma placa eletrônica microcontrolada programável, a fim de converter uma máquina industrial de processo manual para automático. A automação aumentou a produção e resultou em uma qualidade consistente.</p>
-<br>
-<div id="video">
-   <iframe class="responsive-iframe" src="https://www.youtube.com/embed/Bj9OIfq1y4I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</div>`
 let servicos = `
 <h1>Serviços</h1>
 <p><strong>Sistemas Embarcados</strong></p>
 
-<p>Trabalhamos com os microcontroladores e processadores mais difundidos do mercado. Microchip PIC, Atmel AVR (Arduino), ARM Cortex (STM32 e RaspBerry Pi) e Espressif (ESP32 e NodeMCU).<br>Voltado a IoT (internet das coisas), Domótica (automação residencial) e automatização de pequenas máquinas</p>
+<p>Fabricamos PCBs usando os microcontroladores e processadores mais difundidos do mercado. Microchip PIC, Atmel AVR (Arduino), ARM Cortex (STM32 e RaspBerry Pi) e Espressif (ESP32 e NodeMCU).<br>
+Sobre medida para atender as mais diversas necessidades, IoT (internet das coisas), Domótica (automação residencial), automatização de máquinas, interfaces musicais e artísticas.</p>
 
 <p><strong>Desenvolvimento Web</strong></p>
 <p>No desenvolvimento Web usamos as stacks mais populares como HTML5, CSS3, JavaScript, ReactJS, JSON, NodeJS e SQL, na criação de sites, SPAs, Aplicações Web, entre outros.</p>
@@ -28,6 +20,15 @@ let servicos = `
 
 <p><strong>Desenvolvimento de IHMs</strong></p>
 <p>Desenvolvimento de interfaces gráficas e trouch para supervisão e controle de máquinas industriais.</p>`
+let produtos = `
+<h1>Produtos</h1>
+<p><strong>#1 - Automatização do processo industrial de fabricação de Paver e Lajotas.</strong></p>
+
+<p>Nesse projeto foi desenvolvido uma placa eletrônica microcontrolada programável, a fim de converter uma máquina industrial de processo manual para automático. A automação aumentou a produção e resultou em uma qualidade consistente.</p>
+<br>
+<div id="video">
+   <iframe class="responsive-iframe" src="https://www.youtube.com/embed/Bj9OIfq1y4I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>`
 let downloads = `
 <h1>Downloads</h1>
 <p>Algumas páginas ainda estão em desenvolvimento</p>`
@@ -45,12 +46,12 @@ function direciona(bt) {
          txt.innerHTML=sobre
          corBotao(idBotao)
          break;
-      case '2': /* Produtos */
-         txt.innerHTML=produtos
+      case '2': /* Serviços */
+         txt.innerHTML=servicos
          corBotao(idBotao)
          break;
-      case '3': /* Serviços */
-         txt.innerHTML=servicos
+      case '3': /* Produtos */
+         txt.innerHTML=produtos
          corBotao(idBotao)
          break;
       case '4': /* Downloads */
