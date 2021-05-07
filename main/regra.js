1bc25a21a1a59c752b46c9cd1bb9c6981ce4d70d
Daniel Clarinda <57398201+DanielClarinda@users.noreply.github.com>
2021-05-07 13:13:21 -0300

    Remove Fone

--- main/regra.js
+++ main/regra.js
@@ -33,7 +33,7 @@ let downloads = `
 let contato = `
 <h1>Contato</h1>
 <p>Para orçamentos ou sugestões.<br>Atendemos de Segunda a Sexta-feira das 8:00h as 18:00h.</p>
-<p>WhatsApp: (47) 99705-2402 <br>E-mail: <a href="#">dacla.automacao@hotmail.com</a></p>`
+<p>E-mail: <a href="#">dacla.automacao@hotmail.com</a></p>`
 
 function direciona(bt) {
    let txt = document.getElementById("texto")
