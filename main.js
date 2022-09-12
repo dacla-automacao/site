15fb1b3beaa8ef4998a77812ea1a0fe657a7ba84
DanielClarinda <danielitep@hotmail.com>
2022-09-11 21:22:24 -0300

    site v2.0

--- main.js
+++ main.js
@@ -1,79 +1,151 @@
-let sobre = document.getElementById("texto").innerHTML
-let servicos = `
-<h1>Serviços</h1>
+const aboutUs = `
+<div class="item">
+<p>DACLA Automação é uma empresa desenvolvedora de;</p>
+
+<ul class="unstyledList">
+	<li><strong>Sistemas Embarcados</strong></li>
+	<li><strong>Aplicações Web</strong></li>
+	<li><strong>Softwares Multiplataforma</strong></li>
+	<li><strong>Aplicações Mobile</strong></li>
+	<li><strong>Programação de CLPs</strong></li>
+	<li><strong>IHMs</strong></li>
+</ul>
+
+<p>Criada em 2020 pelo CEO Daniel Clarinda. Desenvolvemos sistemas inteligentes e personalizados voltados a automatização de processos. Do hardware a interface do usuário.</p>
+
+<p>Usando stacks modernas conseguimos desenvolver projetos de automatização industriais, residenciais e artísticas de forma rápida, responsiva e multiplataforma.</p>
+</div>`;
+
+const services = `
+<div class="item">
 <h4><strong>Sistemas Embarcados</strong></h4>
-<p>Fabricamos PCBs usando os microcontroladores e processadores mais difundidos do mercado. Microchip PIC, Atmel AVR (Arduino), ARM Cortex (STM32 e RaspBerry Pi) e Espressif (ESP32 e NodeMCU).<br>
-Sobre medida para atender as mais diversas necessidades, IoT (internet das coisas), Domótica (automação residencial), automatização de máquinas, interfaces musicais e artísticas.</p>
 
+<p>Fabricamos placas de circuito impresso(PCBs) usando os microcontroladores e processadores mais difundidos do mercado.<br>
+Tudo sobre medida para atender as mais diversas necessidades, internet das coisas(IoT), automação residencial(Domótica), automação de máquinas, interfaces musicais e artísticas.</p>
+
+<div class="picture">
+	<img src="./images/sistema-embarcado.jpg" alt="Foto de um sistema embarcado">
+</div>
+</div>
+
+<div class="item">
 <h4><strong>Aplicações Web</strong></h4>
-<p>No desenvolvimento Web usamos as stacks mais populares como HTML5, CSS3, JavaScript, ReactJS, JSON, NodeJS e SQL, na criação de sites, SPAs, aplicações Web, games, entre outros.</p>
 
+<p>No desenvolvimento web usamos as stacks mais populares para criar sites, SPAs, Web Apps, games, entre outros.</p>
+
+<div class="picture">
+	<img src="./images/dashboard.png" alt="Imagem de um painel de controle web - Dashboard">
+</div>
+</div>
+
+<div class="item">
 <h4><strong>Softwares Multiplataforma</strong></h4>
-<p>Para sistemas operacionais aproveitamos a beleza e responsividade das tecnologias Web juntamente com framework Electron para resultar em softwares para Windows e Linux.</p>
 
+<p>Aproveitamos a beleza e responsividade das tecnologias Web para desenvolver programas para sistemas operacionais Windows e Linux.</p>
+
+<div class="picture">
+	<img src="./images/weather.jpg" alt="Exemplo que software desenvolvido com electron - Weather">
+</div>
+</div>
+
+<div class="item">
 <h4><strong>Aplicações Mobile</strong></h4>
-<p>Desenvolvimento de Apps Nativos para smartphones com React Native.</p>
 
-<h4><strong>Automação Industrial - Programação de CLPs</strong></h4>
-<p>Automatização de máquinas industriais com controladores lógicos programáveis na linguagem Ladder.</p>
+<p>Desenvolvimento de Aplicativos Nativos para smartphones Android e iOS.</p>
 
+<div class="picture">
+	<img src="./images/mobile-app.png" alt="Exemplo de aplicações mobile">
+</div>
+</div>
+
+<div class="item">
+<h4><strong>Programação de CLPs</strong></h4>
+
+<p>Automatização de máquinas industriais com controladores lógicos programáveis(CLPs).</p>
+
+<div class="picture">
+	<img src="./images/clp-ladder.jpg" alt="CLP Clic02 WEG progamado em Ladder">
+</div>
+</div>
+
+<div class="item">
 <h4><strong>Desenvolvimento de IHMs</strong></h4>
-<p>Desenvolvimento de interfaces gráficas e trouch para supervisão e controle de processos industriais.</p>`
-let produtos = `
-<h1>Produtos</h1>
-<h4><strong>#1 - Automatização do processo industrial de fabricação de Paver e Lajotas.</strong></h4>
-<p>Nesse projeto foi desenvolvido uma placa eletrônica microcontrolada programável, a fim de converter uma máquina industrial de processo manual para automático. A automação aumentou a produção e resultou em uma qualidade consistente.</p>
+
+<p>Desenvolvimento de interfaces gráficas e trouch para supervisão e controle de processos industriais.</p>
+
+<div class="picture">
+	<img src="./images/supervisorio.jpg" alt="Sistema supervisório de cervejaria">
+</div>
+</div>`;
+
+const products = `
+<div class="item">
+<h4><strong>Automação do processo de fabricação de Paver e Lajotas.</strong></h4>
+
+<p>Foi desenvolvido um controlador programável, a fim de converter a máquina originalmente de processo manual para automático. A automação aumentou a produção e resultou em uma maior consistência da qualidade do produto.</p>
 <br>
-<div id="video">
-   <iframe class="responsive-iframe" src="https://www.youtube.com/embed/Bj9OIfq1y4I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<div id='video'>
+	<iframe class='responsive-iframe' src='https://www.youtube.com/embed/Bj9OIfq1y4I' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>
+</div>
 </div>`
-let downloads = `
-<h1>Downloads</h1>
-<p>Desculpe!</p>
-<p>Algumas páginas ainda estão em desenvolvimento.</p>`
-let contato = `
-<h1>Contato</h1>
-<p>Para orçamentos ou sugestões.<br>Atendemos de Segunda a Sexta-feira das 8:00h as 18:00h.</p>
-<p>E-mail: <a href="#">dacla.automacao@hotmail.com</a></p>`
+;
 
-function direciona(bt) {
-   let txt = document.getElementById("texto")
-   let idBotao = bt.id
+const downloads = `
+<div class="item">
+<p>Nenhum item disponível</p>
+</div>`
+;
 
-   switch (idBotao) {
-      case '1': /* Sobre Nós */
-         txt.innerHTML=sobre
-         corBotao(idBotao)
-         break;
-      case '2': /* Serviços */
-         txt.innerHTML=servicos
-         corBotao(idBotao)
-         break;
-      case '3': /* Produtos */
-         txt.innerHTML=produtos
-         corBotao(idBotao)
-         break;
-      case '4': /* Downloads */
-         txt.innerHTML=downloads
-         corBotao(idBotao)
-         break;
-      case '5': /* Contato */
-         txt.innerHTML=contato
-         corBotao(idBotao)
-         break;
-      default:
-         txt.innerHTML="ERROR - button value"
-   }
+const contact = `
+<div class="item">
+<h4>Para orçamentos ou sugestões.</h4>
+
+<p>Atendemos de Segunda a Sexta-feira das 8:00h as 18:00h.</p>
+<br>
+<p>Fone (WhatsApp): (47) 9 9705-2402</p>
+<p>E-mail: <a href='#'>dacla.automacao@hotmail.com</a></p>
+<br>
+<p> Dacla Automação no <a href="https://www.youtube.com/channel/UChazDXIuQ71YFWU7EQbSK5w" target="_blank"><strong>YouTube</strong></a> & <a href="http://instagram.com/daclaautomacao" target="_blank"><strong>Instagram</strong></a></p>
+</div>`
+;
+
+function selectedStyleControl() {
+	let selectedList = document.querySelectorAll(".selected");
+
+	selectedList.forEach((selectedItem)=>{
+		document.getElementsByName(selectedItem.name)[0].className = "noSelected";
+	})
 }
 
-function corBotao(bt) {
-   for(let i = 1; i <=5; i++) {
-      if(bt != i) {
-         document.getElementById(i).style.backgroundColor="#26538A"
-         document.getElementById(i).style.color="white"
-      } else {
-         document.getElementById(bt).style.backgroundColor="white"
-         document.getElementById(i).style.color="black"
-      }
-   }
-}
+function handleContent(option) {
+	let content = "";
+	
+	selectedStyleControl();
+	
+	document.getElementsByName(option.name)[0].className = "selected";
+	
+	switch (option.name) {
+		case "aboutUs":
+			content = aboutUs;
+			break;
+		case "services":
+			content = services;
+			break;
+		case "products":
+			content = products;
+			break;
+		case "downloads":
+			content = downloads;
+			break;
+		case "contact":
+			content = contact;
+			break;
+		default:
+			content = "Erro - handleContent('invalid')";
+	}
+
+	document.querySelector(".content").innerHTML = content;
+};
+
+const firstMenuOption = document.querySelector("nav > a");
+handleContent(firstMenuOption);
