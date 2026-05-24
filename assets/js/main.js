f827e586f28498d61134f70aaca1757f8d94afce
Daniel Clarinda <57398201+DanielClarinda@users.noreply.github.com>
2026-05-24 08:28:51 -0300

    upgrade

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,7 @@
 import { initNav } from './modules/nav.js';
 import { initYear } from './modules/year.js';
-import { initScrollSpy } from './modules/scrollspy.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initNav();
   initYear();
-  initScrollSpy();
 });
